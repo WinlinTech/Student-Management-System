d78eab0954c7d7950208da052f84be8e4de36361
sid120 <siddharthrakshe12@gmail.com>
2023-08-28 23:44:07 +0530

    created navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import SignUp from './Pages/Login_SignUp/SignUp';
 import {BrowserRouter, Routes,Route} from 'react-router-dom';
 import Login from './Pages/Login_SignUp/Login';
 import Home from './Pages/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
 
 
 function App() {
@@ -11,12 +13,17 @@ function App() {
 
   return (
       <BrowserRouter>
-        <Routes>
-             <Route path='/register' element={<SignUp/>}/>
-             <Route path='/login' element={<Login/>}/>
-             <Route path='/home' element={<Home/>}/>
+      <Navbar />
+      <Footer/>
 
+      <div className="pages">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
+      </div>
+        
       </BrowserRouter>
   )
 }
